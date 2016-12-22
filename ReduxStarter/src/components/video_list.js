9ea9f2acfeef5bc0bc651eb78fbc2f87e0d69025
Jeffrey <jfflee.22@gmail.com>
2016-12-22 15:27:36 -0800

    add component in video_list and video_list item to return list of thumbnails and title of a yt search

--- ReduxStarter/src/components/video_list.js
+++ ReduxStarter/src/components/video_list.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
-const VideoList => {
+const VideoList = (props) => {
+  const videoItems = props.videos.map((video) => {
+  return <VideoListItem key={video.etag} video={video} />
+});
+
+  const videos = props.videos;
   return (
-    <ul>
+    <ul className="col-md-4 list-group">
+      {videoItems}
     </ul>
   );
-}
+};
+
+export default VideoList;
--- ReduxStarter/src/index.js
+++ ReduxStarter/src/index.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY = "AIzaSyBXnEJjnhdACEkJdIpreisvfW8OjsmnBOE";
 
 
@@ -24,6 +23,7 @@ class App extends Component {
     return (
       <div>
         Search: <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
