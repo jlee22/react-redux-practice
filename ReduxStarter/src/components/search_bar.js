6e074507a89494ecac617972c0ff486a8c717017
Jeffrey <jfflee.22@gmail.com>
2016-12-21 17:21:00 -0800

    change functional object to class object for searchbar

--- ReduxStarter/src/components/search_bar.js
+++ ReduxStarter/src/components/search_bar.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
-const SearchBar = () => {
-  return <input />;
-};
+class SearchBar extends React.Component {
+  render() {
+    return <input />;
+  }
+}
 
 export default SearchBar;
