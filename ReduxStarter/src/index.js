f40a765acb13191e7ad58cb70ae6e6445b75012d
Jeffrey <jfflee.22@gmail.com>
2016-12-21 17:17:13 -0800

    add env for youtube key and add import searchbar

--- ReduxStarter/src/index.js
+++ ReduxStarter/src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBXnEJjnhdACEkJdIpreisvfW8OjsmnBOE';
 
 // Create new component to produce some html
 const App = () => {
    //JSX - this gets transpiled to vanilla js
-   return <div>Hi!</div>;
+  return (
+    <div>
+      Search: <SearchBar />
+    </div>
+  );
 }
 
 // Take generated html and put it on the page (in the DOM)
