be91bd54aa6cad939171dfc92c184e2cb6249029
Jeffrey <jfflee.22@gmail.com>
2016-12-21 17:51:10 -0800

    input now returns state value

--- ReduxStarter/src/components/search_bar.js
+++ ReduxStarter/src/components/search_bar.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class SearchBar extends React.Component {
-  render() {
-    return <input />;
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: 'Starting Value' };
   }
+
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value })} />
+      </div>
+    );
+  }
+
 }
 
 export default SearchBar;
