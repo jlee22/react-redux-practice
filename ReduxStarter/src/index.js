64f425fe70f58b6e3da5cf666e6945701bfd4a34
Jeffrey <jfflee.22@gmail.com>
2016-12-21 16:50:24 -0800

    app function renders div onto index container

--- ReduxStarter/src/index.js
+++ ReduxStarter/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
 
 // Create new component to produce some html
-
-const App = function() {
+const App = () => {
    //JSX - this gets transpiled to vanilla js
    return <div>Hi!</div>;
 }
 
 // Take generated html and put it on the page (in the DOM)
-
-React.render(App);
+ReactDOM.render(<App />, document.querySelector('.container'));
