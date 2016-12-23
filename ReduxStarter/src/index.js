249984206f25ea3ecbb1892d36ba92e46e1b8b21
Jeffrey <jfflee.22@gmail.com>
2016-12-22 16:04:31 -0800

    complete callback from app to videolist to videodetails for onvideoselect functionality

--- ReduxStarter/src/index.js
+++ ReduxStarter/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = "AIzaSyBXnEJjnhdACEkJdIpreisvfW8OjsmnBOE";
 
 
@@ -11,11 +12,16 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
     YTSearch({key: API_KEY, term: 'hotdogs'}, (videos) => {
-      this.setState({ videos});
-      // ^ same as this.setState({ videos: videos });
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
@@ -23,7 +29,8 @@ class App extends Component {
     return (
       <div>
         Search: <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo})} videos={this.state.videos} />
       </div>
     );
   }
