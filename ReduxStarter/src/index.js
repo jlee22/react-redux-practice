856882a8eb3aac9028e2a83a73f1dbf660700586
Jeffrey <jfflee.22@gmail.com>
2016-12-22 16:21:24 -0800

    add functionality for searchbar to update term and search using new term

--- ReduxStarter/src/index.js
+++ ReduxStarter/src/index.js
@@ -17,7 +17,11 @@ class App extends Component {
       selectedVideo: null
     };
 
-    YTSearch({key: API_KEY, term: 'hotdogs'}, (videos) => {
+    this.videoSearch('sushi');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
@@ -28,7 +32,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo})} videos={this.state.videos} />
       </div>
