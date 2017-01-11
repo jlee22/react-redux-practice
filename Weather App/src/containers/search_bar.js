32b5c9f6416a67f4b4eb342c5f34f930b9421b2b
Jeffrey <jfflee.22@gmail.com>
2017-01-11 12:09:56 -0800

    add state to search bar, also move button from app to search bar

--- Weather App/src/containers/search_bar.js
+++ Weather App/src/containers/search_bar.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
   render() {
     return (
       <form className="input-group">
         <input />
+        <span>
+          <button type="submit" className="btn btn-secondary">Search</button>
+        </span>
       </form>
     );
   }
