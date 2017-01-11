e2a748849cd4a3b7386ae6ce1615b88233496d45
Jeffrey <jfflee.22@gmail.com>
2017-01-11 13:47:18 -0800

    add oninputchange to set value of search bar

--- Weather App/src/containers/search_bar.js
+++ Weather App/src/containers/search_bar.js
@@ -5,15 +5,23 @@ export default class SearchBar extends Component {
     super(props);
 
     this.state = { term: '' };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value);
+    this.setState({ term: event.target.value });
   }
 
   render() {
     return (
       <form className="input-group">
         <input
-        placeholder="Get a five-day forecast in your favorite cities"
-        className='form-control'
-        />
+          placeholder="Get a five-day forecast in your favorite cities"
+          className='form-control'
+          value={this.state.term}
+          onChange={this.onInputChange} />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Search</button>
         </span>
