2035438ac80a2a2c958eb31fd5bc36dac27e1205
Jeffrey <jfflee.22@gmail.com>
2017-01-11 15:16:41 -0800

    add root url default us country

--- Weather App/src/actions/index.js
+++ Weather App/src/actions/index.js
@@ -1,1 +1,11 @@
 const API_KEY = '3ba6f382faa642444091af739525d3d8';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchweather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+
+  return {
+    type: FETCH_WEATHER
+  };
+}
