67ee5c083ec83a6d096021e7c9cfffefb368825a
Jeffrey <jfflee.22@gmail.com>
2017-01-12 13:26:32 -0800

    convert kelvin to f, add average line and values, adisplay units

--- Weather App/src/containers/weather_list.js
+++ Weather App/src/containers/weather_list.js
@@ -5,13 +5,15 @@ import Chart from '../components/chart';
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
-    const temps = cityData.list.map(weather => weather.main.temp)
+    const temps = cityData.list.map(weather => weather.main.temp*9/5-459.67)
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
     return (
       <tr key={name}>
         <td>{name}</td>
-        <td>
-          <Chart data={temps} color="orange" />
-        </td>
+        <td><Chart data={temps} color="orange" units="K"/></td>
+        <td><Chart data={pressures} color="orange" units ="hPa" /></td>
+        <td><Chart data={humidities} color="orange" units="%" /></td>
       </tr>
     );
   }
@@ -22,9 +24,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (F)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
