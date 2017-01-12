a74bbd3b25b36917fe029a6443eeb14569fb7539
Jeffrey <jfflee.22@gmail.com>
2017-01-11 17:46:13 -0800

    move sparkline section to a component

--- Weather App/src/containers/weather_list.js
+++ Weather App/src/containers/weather_list.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
@@ -10,9 +10,7 @@ class WeatherList extends Component {
       <tr key={name}>
         <td>{name}</td>
         <td>
-          <Sparklines height ={120} width={180} data={temps}>
-            <SparklinesLine color="orange" />
-          </Sparklines>
+          <Chart data={temps} color="orange" />
         </td>
       </tr>
     );
