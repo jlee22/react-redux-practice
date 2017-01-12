29978febb5289ba863233a39b4bc786141fa2b63
Jeffrey <jfflee.22@gmail.com>
2017-01-11 17:08:05 -0800

    add unique key for each city

--- Weather App/src/containers/search_bar.js
+++ Weather App/src/containers/search_bar.js
@@ -14,7 +14,6 @@ class SearchBar extends Component {
   }
 
   onInputChange(event) {
-    console.log(event.target.value);
     this.setState({ term: event.target.value });
   }
 
@@ -30,7 +29,7 @@ class SearchBar extends Component {
     return (
       <form onSubmit={this.onFormSubmit} className="input-group">
         <input
-          placeholder="Get a five-day forecast in your favorite cities"
+          placeholder="Get a five-day forecast of a city"
           className='form-control'
           value={this.state.term}
           onChange={this.onInputChange} />
--- Weather App/src/containers/weather_list.js
+++ Weather App/src/containers/weather_list.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class WeatherList extends Component {
+class WeatherList extends Component {
+  renderWeather(cityData) {
+    const name = cityData.city.name;
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+      </tr>
+    );
+  }
+
   render() {
     return (
       <table className="table table-hover">
@@ -13,8 +23,15 @@ export default class WeatherList extends Component {
           </tr>
         </thead>
         <tbody>
+          {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     );
   }
 }
+
+function mapStateToProps({ weather }) {
+  return { weather };
+}
+
+export default connect(mapStateToProps)(WeatherList);
