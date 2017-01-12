7293b86166ed95a097dc6641ea4b53c99befd3fa
Jeffrey <jfflee.22@gmail.com>
2017-01-11 16:27:21 -0800

    add weatherlist container with table header

---
+++ Weather App/src/containers/weather_list.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class WeatherList extends Component {
+  render() {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
+          </tr>
+        </thead>
+        <tbody>
+        </tbody>
+      </table>
+    );
+  }
+}
