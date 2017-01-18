2815639aee35f376debc3cc8357111b222ba064b
Jeffrey <jfflee.22@gmail.com>
2017-01-17 16:41:16 -0800

    define initial state with list of posts and active post

--- Blog App/src/reducers/reducer_posts.js
+++ Blog App/src/reducers/reducer_posts.js
@@ -1,6 +1,6 @@
 import { FETCH_POSTS } from '../actions/index';
 
-const INITIAL_STATE = {};
+const INITIAL_STATE = { all:[], post: null };
 
 export default function(state = INITIAL_STATE, action) {
   switch(action.type) {
