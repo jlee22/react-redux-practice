651957b9e3ef0366a8b2a2a53b273786774eaedc
Jeffrey <jfflee.22@gmail.com>
2017-01-17 17:30:00 -0800

    lay down boilder plate for new posts route

---
+++ Blog App/src/components/posts_new.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PostsNew extends Component {
+  render () {
+    return (
+      <div>Create Post Form</div>
+    );
+  }
+}
+
+export default PostsNew;
