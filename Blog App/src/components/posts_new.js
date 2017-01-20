034c9583b2224cc5fc4c58efb31f2ae73307be21
Jeffrey Lee <jfflee.22@gmail.com>
2017-01-20 15:36:04 -0800

    add action creator for new post and hook it up to handlesubmit

--- Blog App/src/components/posts_new.js
+++ Blog App/src/components/posts_new.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
   render () {
@@ -9,7 +10,7 @@ class PostsNew extends Component {
     // console.log(title);
 
     return (
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(this.props.createPost)}>
         <h3>Create Post</h3>
         <div className="form-group">
           <label>Title</label>
@@ -32,7 +33,10 @@ class PostsNew extends Component {
   }
 }
 
+//connect: first argument is mapStateToProps, 2nd is mapDispatchToProps
+// reduxForm: 1st is form config, 2nd is mapStateToProps, 3rd is mapDispatchToProps
+
 export default reduxForm({
   form: 'PostsNewForm',
-  field: ['title', 'categories', 'content']
-})(PostsNew);
+  fields: ['title', 'categories', 'content']
+}, null, { createPost })(PostsNew);
