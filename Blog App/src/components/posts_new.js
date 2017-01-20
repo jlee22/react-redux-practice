50b738c713013e348dce426cd6e07c36cf702cee
Jeffrey Lee <jfflee.22@gmail.com>
2017-01-20 15:28:09 -0800

    pass form input

--- Blog App/src/components/posts_new.js
+++ Blog App/src/components/posts_new.js
@@ -3,23 +3,27 @@ import { reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   render () {
-    return (
-      <form>
-        <h3>Create Post</h3>
+    const { fields: { title, categories, content }, handleSubmit } = this.props;
+    // es5 syntax would be something like
+    // const title = this.props.fields.title;
+    // console.log(title);
 
+    return (
+      <form onSubmit={handleSubmit}>
+        <h3>Create Post</h3>
         <div className="form-group">
           <label>Title</label>
-          <label type="text" className="form-control" />
+          <label type="text" className="form-control" {...title} />
         </div>
 
         <div className="form-group">
           <label>Categories</label>
-          <label type="text" className="form-control" />
+          <label type="text" className="form-control" {...categories} />
         </div>
 
         <div className="form-group">
           <label>Content</label>
-          <label type="text" className="form-control" />
+          <textarea className="form-control" {...content} />
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>
