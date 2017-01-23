badce346e2ea77b899bd1e8fff8b482e270186b5
Jeffrey Lee <jfflee.22@gmail.com>
2017-01-22 16:13:03 -0800

    reconfigure post index to display a list of post title

--- Blog App/src/components/posts_new.js
+++ Blog App/src/components/posts_new.js
@@ -1,9 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
 import { Link } from 'react-router';
 
 class PostsNew extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
+  onSubmit(props) {
+    this.props.createPost(props)
+      .then(() => {
+        // blog post has been created, navigate the user to the index
+        // We navigate by calling this.context.router.push with the new path to navigate to
+        this.context.router.push('/');
+      });
+  }
+
   render () {
     const { fields: { title, categories, content }, handleSubmit } = this.props;
     // es5 syntax would be something like
@@ -11,7 +24,7 @@ class PostsNew extends Component {
     // console.log(title);
 
     return (
-      <form onSubmit={handleSubmit(this.props.createPost)}>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <h3>Create Post</h3>
         <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
           <label>Title</label>
