663471057e03785ac247cd9389f8f43864be1fc5
Jeffrey Lee <jfflee.22@gmail.com>
2017-01-23 18:22:47 -0800

    add posts show component

--- Blog App/src/routes.js
+++ Blog App/src/routes.js
@@ -4,6 +4,7 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
 
 export default (
   <Route path="/" component={App}>
