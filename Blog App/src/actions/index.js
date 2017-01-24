7879b277807543e475cb4f37a75065ace87cc80a
Jeffrey Lee <jfflee.22@gmail.com>
2017-01-24 14:49:38 -0800

    add link to post index from show

--- Blog App/src/actions/index.js
+++ Blog App/src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
 export const FETCH_POST = 'FETCH_POST';
+// export const DELETE_POST = 'DELETE_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=reactandreduxisfun22'
--- Blog App/src/components/posts_show.js
+++ Blog App/src/components/posts_show.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsShow extends Component {
   componentWillMount() {
     this.props.fetchPost(this.props.params.id);
   }
   render() {
-    if (!this.props.post) {
+    const { post } = this.props;
+    if (!post) {
       return <div>Loading...</div>
     }
-    return <div>Show post {this.props.params.id}</div>;
+    return (
+      <div>
+        <h3>{post.title}</h3>
+        <h6>Category: {post.categories}</h6>
+        <p>{post.content}</p>
+        <Link to="/">&#60;&#60; Back to Index</Link>
+      </div>
+    );
   }
 }
 
